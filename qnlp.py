cebb078cd4d59a225fd957fdca7b692d9639b1f4
Cindyzzz616 <cxy.zhang@mail.utoronto.ca>
2026-04-12 14:45:11 -0400

    last bit of qnlp

--- qnlp.py
+++ qnlp.py
@@ -1,6 +1,6 @@
 from lambeq import BobcatParser
 
 parser = BobcatParser("/Users/cindyzhang/LIN402-Conlang/.cache/lambeq/bobcat")
-diagram = parser.sentence2diagram("That time, the language of all places is one")
+diagram = parser.sentence2diagram("While people moved to east, they found a plain around Shinar and settled in there")
 print(diagram)
-diagram.draw(path="diagram.png", show=False)
+diagram.draw(path="diagram2.png", show=False)
